4c84cac6a21824ac75705bb1b07d15bec9e09c73
John Wasielewicz <wazjohn@yahoo.com>
2020-07-31 18:05:03 -0500

    Searching task accomplished.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,10 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
+
   constructor() {
+    this.displayList = [];
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
@@ -24,9 +27,26 @@ export class AppComponent {
           // TODO: loop over satellites
           // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
           // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
+          
+          // make a copy of the sourceList to be shown to the user
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
- }
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+  }
+
 }
